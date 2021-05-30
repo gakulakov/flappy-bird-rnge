397ce6231187b27b3f2d144c46d7aa2bb92a7dbd
gakulakov <gakulakov.ssx@gmail.com>
2021-05-31 01:44:18 +0300

    worked game

---
+++ Test/Finger.jsx
@@ -0,0 +1,20 @@
+import React, {PureComponent} from 'react'
+
+
+export default class Box extends PureComponent {
+    render() {
+        const size = 100
+        const x = this.props.x - size / 2
+        const y = this.props.y - size / 2
+        return (
+            <div style={{
+                position: 'absolute',
+                width: size,
+                height: size,
+                backgroundColor: 'red',
+                left: x,
+                top: y
+            }} />
+        )
+    }
+}
---
+++ components/Pipe.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {View} from "react-native";
+
+
+export const Pipe = (props) => {
+    const width = props.size[0]
+    const height = props.size[1]
+    const x = props.body.position.x - width / 2
+    const y = props.body.position.y - height / 2
+
+    return (
+        <View
+            style={{
+                position: 'absolute',
+                top: y,
+                left: x,
+                width: width,
+                height: height,
+                backgroundColor: props.color
+            }}
+        />
+    )
+}
